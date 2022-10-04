3f26e0355c96c51da4654be72b1676a8bbbf4ebb
Jefferson Pacheco <andres.pacheco.97@gmail.com>
2022-10-04 12:38:03 -0500

    Creación de métodos con ciclos diferentes para recorren un mismo arreglo

---
+++ Entities/Course.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreSchool.Entities
+{
+    public class Course
+    {
+        //Set name and get a name
+        public string name { get; set; }
+        //Set name and get a unique Id
+        public string uniqueId { get; private set; }
+        //Set name and get a shift
+        public ShiftTypes shift { get; set; }
+
+    //Course constructor, generates a random ID with a build-in method 
+    public Course() =>uniqueId = Guid.NewGuid().ToString();
+    
+    }
+}
---
+++ Entities/ShiftTypes.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreSchool.Entities
+{
+    public enum ShiftTypes
+    {
+        Morning,
+        Afternoon,
+        Evening,
+    }
+}
